e751208b856cc4af507300c371f5d2ba04f9305e
ludy-neita <luyas9008@hotmail.com>
2023-06-10 17:55:58 -0500

    Ajuste de App

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Series from "./pages/Series";
 import Comics from "./pages/Comics";
 import Error404 from "./pages/Error404";
 import Header from "./components/Header/Header";
-import useFetch from "./hooks/useFetch";
+//import useFetch from "./hooks/useFetch";
 
 function App() {
   //const marvelApi = useFetch ('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=cb144388a602ec642946106969fe3eff&hash=1448e4152ea7fe6ac11f8e825d92b22e');
